25a02ac35512fb88176f8e61745384de40a09c85
KRGurjar <62748373+KRGurjar@users.noreply.github.com>
2020-06-07 18:46:16 +0530

    Delete rootreducer.js

--- Picture Perfect/client/src/redux/rootreducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import reducer from './MovieList/MovieReducer.js'
-
-const rootReducer = combineReducers ({
-	 movies : reducer
-	)}
- 
- export defaul rootReducer
